f6e687cfe71cafe0242b1014590398b00ce7ebc2
unknown <unknown@example.com>
2023-07-01 18:13:11 +0800

    0.1
    如何使用for自动化处理eventlist

--- pages/timeline/timeline.js
+++ pages/timeline/timeline.js
@@ -26,18 +26,20 @@ class myWeek{
     }
 }
 
-/**用于记录活动 */
+/**List记录一周的数据，其元素eventList用于记录每日的活动，并由this.setData向Data中的List输入数据 */
+var List = new Array(6);
+var eventList = new Array();
 class myEvent{
-    constructor(eventName,eventDate,eventDaily,eventWeekly,eventMonthly,eventTime){
-        this.name = eventName;
+    constructor(brandname,eventName,eventLogo,eventDate,eventWeekly,eventMonthly,eventTime){
+        this.brandname = brandname
+        this.eventname = eventName;
+        this.logo = eventLogo;
         /**活动日期：
          * date特定某天，类型为Date或0，
-         * daily每日，类型为Boolen，
          * weekly每周，类型为Array[0,1,2,3,4,5,6]，0为周日，
          * monthly每月，类型为Array[1~31]，注意边界问题
          * 时间为0时不在时间轴上显示具体时间 */
         this.date = eventDate;
-        this.daily = eventDaily;
         this.weekly = eventWeekly;
         this.monthly = eventMonthly;
         this.time = eventTime;
@@ -46,39 +48,40 @@ class myEvent{
 
 Page({
 
-    /**
-     * 页面的初始数据
+    /**页面的初始数据
+     * Month,Week,Day传输日期数据
+     * current传输当前页面数据
+     * List传输活动列表
      */
     data: {
         Month : [0],
         Week : [0],
         Day : [0],
         swiperCurrent : 0,
-        eventList:[
-            'KFC',
-            'McDonald',
-            'StarBucks'
-        ]
+        List:[],
     },
 
-    /**wxs调用的callMethod函数，用于同步header与swiper状态 */
-    syncSwiper: function(i){
-        this.setData({swiperCurrent: i,});
-        return false;
-    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
       /**init */
       let init = new function(){
+        /**初始化一周的日期数据 */
         let week = new myWeek(nowDate);
         week.setNextWeek();
+        /**初始化一周的活动数据，在JSON中配置 */
+        let event0 = new myEvent('KFC','疯狂星期四','/static/images/logo/Kfc_logo.png',0,4,0,'10:30');
+        let event1 = new myEvent('McDonalds','麦当劳会员日','/static/images/logo/McDonalds-logo.png',0,[0,6],0,"10:30");
+        eventList[0] = event0;
+        eventList[1] = event1;
+        List[0] = eventList;
       }
       this.setData({
           Month : Month,
           Week : Week,
           Day : Day,
+          List : List,
       });
       /**test */
 
@@ -132,5 +135,11 @@ Page({
      */
     onShareAppMessage() {
 
-    }
+    },
+
+    /**wxs调用的callMethod函数，用于同步header与swiper状态 */
+    syncSwiper: function(i){
+        this.setData({swiperCurrent: i,});
+        return false;
+    },
 })
